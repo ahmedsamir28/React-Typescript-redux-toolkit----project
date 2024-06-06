408ba14da847b28acf33e38129f8e57827c3877f
Ahmed Samir <155876773+ahmedsamir28@users.noreply.github.com>
2024-06-06 03:05:56 +0300

    page details  ui

--- src/Pages/PageDetails.tsx
+++ src/Pages/PageDetails.tsx
@@ -1,9 +1,20 @@
-interface IProps  {}
+interface IProps { }
 
 const PageDetails = () => {
   return (
-    <div>DetailsPage</div>
-  )
+    <div className="card card-side bg-base-100 shadow-xl my-10 border w-9/12 container ">
+      <figure><img src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie" /></figure>
+      <div className="card-body">
+        <h6>category</h6>
+        <h2 className="card-title">New movie is released!</h2>
+        <p>Click the button to watch on Jetflix app.</p>
+
+        <div className="card-actions justify-end flex items justify-between">
+        <span>$8000</span>
+          <button className="btn btn-primary">add to cart</button>
+        </div>
+      </div>
+    </div>)
 }
 
 export default PageDetails
