c30bfa6954388dbb6a6c83a528f918bd6a2b478e
Ahmed Samir <155876773+ahmedsamir28@users.noreply.github.com>
2024-06-11 06:41:11 +0300

    end of register page ui

--- src/Pages/RegisterPage.tsx
+++ src/Pages/RegisterPage.tsx
@@ -1,3 +1,10 @@
+import Button from "../UI-items/Button"
+import Input from "../UI-items/Input"
+import { useForm, SubmitHandler } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import InputErrorMessage from "../UI-items/InputErrorMessage";
+import { registerSchema } from "../validation";
+import { REGISTER_FORM } from "../data";
 
 interface IFormInput {
     username: string;
@@ -6,9 +13,42 @@ interface IFormInput {
 }
 
 const RegisterPage = () => {
+    const { register, handleSubmit, formState: { errors }, } = useForm<IFormInput>({
+        resolver: yupResolver(registerSchema),
+    });
+    const onSubmit: SubmitHandler<IFormInput> = async (data) => {
+        console.log("DATA", data);
+    }
+
+    const renderLoginForm = REGISTER_FORM.map(({ name, placeholder, type, validation }, idx) => {
+        return (
+            <div key={idx}>
+                <Input
+                    type={type}
+                    placeholder={placeholder}
+                    {...register(name, validation)}
+                />
+                {errors[name] && <InputErrorMessage message={errors[name]?.message} />}
+
+            </div>
+        );
+    }
+    );
     return (
-        <div>
-            logim page
+        <div className="max-w-md my-20 mx-auto">
+            <h2 className="text-center mb-4 text-3xl font-semibold">
+                Login to get access!
+            </h2>
+            <form onSubmit={handleSubmit(onSubmit)}
+                className="space-y-4  bg-base-200 border-2 border-base-300  rounded-lg p-5" >
+                <div className="flex flex-col gap-3">
+                    {renderLoginForm}
+                </div>
+
+                <Button className="bg-warning hover:bg-amber-500 font-medium capitalize px-3 py-2" width="w-full">
+                    Login
+                </Button>
+            </form>
         </div>
     )
 }
